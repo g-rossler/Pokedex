ea06b3b9be8c8d4f02ef4375980954af8003dd58
warthundervlk <warthundervlk@gmail.com>
2021-10-20 17:56:00 -0300

    cambios

--- src/main.js
+++ src/main.js
@@ -8,83 +8,57 @@ function asignarNombres(resultado){
 }
 
 function asignarCodigo(resultado){
-    let codigo = document.querySelectorAll("#especificaciones-codigo")
-    codigo.forEach((element, index) => {
-        element.textContent = resultado.results[index]
-    })
+    let codigo = document.querySelector("#especificaciones-codigo-valor")
+    codigo.textContent = resultado.id
 }
 
+function asignarImagen(resultado) {
+    let $imagen = document.querySelector("#especificaciones-imagen")
+    $imagen.src = resultado.sprites.front_default
+}
 
-function asignarImagen(resultado){
-    let imagen = document.querySelector("#especificaciones-imagen")
+function asignarDatosPokemon(resultado){
+    let $spanTipo = document.querySelector("#especificaciones-tipo-valor")
+    let $spanAltura = document.querySelector("#especificaciones-altura-valor")
+    let $spanVida = document.querySelector("#especificaciones-vida-valor")
+    let $spanAtaque = document.querySelector("#especificaciones-ataque-valor")
+    let $spanDefensa = document.querySelector("#especificaciones-defensa-valor")
+    let $spanPeso = document.querySelector("#especificaciones-peso-valor")
+    let $spanAtaqueEspecial = document.querySelector("#especificaciones-ataque-especial-valor")
+    let $spanDefensaEspecial = document.querySelector("#especificaciones-defensa-especial-valor")
+    let $spanVelocidad = document.querySelector("#especificaciones-velocidad-valor")
+
+    $spanAltura.textContent = resultado.height
+    $spanVida.textContent = resultado.stats[0].base_stat
+    $spanAtaque.textContent = resultado.stats[1].base_stat
+    $spanDefensa.textContent = resultado.stats[2].base_stat
+    $spanPeso.textContent = resultado.weight
+    $spanAtaqueEspecial.textContent = resultado.stats[3].base_stat
+    $spanDefensaEspecial.textContent = resultado.stats[4].base_stat
+    $spanVelocidad.textContent = resultado.stats[5].base_stat
+
+    let texto = ""
+
+   resultado.types.forEach(elemento => {
+        texto = texto + " " + elemento.type.name[0].toUpperCase() + elemento.type.name.slice(1)
+    } )
+    
+    $spanTipo.textContent = texto
+}
+
+function asignarDatos(resultado){
+    
         let link = resultado.results[0].url;
         let linkMejorado = link.replace("https://pokeapi.co/api/v2", "https://pokeapi-215911.firebaseapp.com/api/v2")
         fetch(linkMejorado)
         .then(resultado1 => resultado1.json())
         .then(resultado1JSON => {
-            imagen.src = resultado1JSON.sprites.front_default
+            asignarImagen(resultado1JSON)
+            asignarDatosPokemon(resultado1JSON)
+            asignarCodigo(resultado1JSON)
         })
         .catch(error => console.error("error", error))
 }
-
-
-
-function ultimaPagina() {
-    fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=1100`)
-        .then(resultado => resultado.json())
-        .then(resultadoJSON => {
-
-
-            asignarNombres(resultadoJSON);
-            asignarImagenes(resultadoJSON);
-            document.querySelector("#numero-pagina").innerText = 56
-
-        })
-        .catch(error => console.error("error", error));
-}
-
-function controlPagina() {
-    let pagina = Number(document.querySelector("#numero-pagina").innerText)
-    if(pagina > 1) {
-        anteriorPagina()
-    } else {
-        return false
-    }
-}
-
-function siguientePagina() {
-    let pagina = Number(document.querySelector("#numero-pagina").innerText) * 20
-    fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${pagina}`)
-        .then(resultado => resultado.json())
-        .then(resultadoJSON => {
-
-            asignarNombres(resultadoJSON);
-            asignarImagenes(resultadoJSON);
-            document.querySelector("#numero-pagina").innerText = (pagina / 20) + 1
-
-        })
-        .catch(error => console.error("error", error));
-
-}
-
-function anteriorPagina() {
-    let pagina = (Number(document.querySelector("#numero-pagina").innerText) - 2) * 20
-    fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${pagina}`)
-        .then(resultado => resultado.json())
-        .then(resultadoJSON => {
-
-
- 
-            asignarNombres(resultadoJSON);
-            asignarImagenes(resultadoJSON);
-            document.querySelector("#numero-pagina").innerText = (pagina / 20) + 1
-
-        })
-        .catch(error => console.error("error", error));
-
-}
-
-
 
 function asignarNombre(resultado) {
     let nombre = document.querySelector("#especificaciones-nombre")
@@ -97,16 +71,28 @@ function datosPrimeraPagina() {
         .then(resultadoJSON => {
 
 
-            asignarCodigo(resultadoJSON)
+            
             asignarNombres(resultadoJSON);
             asignarNombre(resultadoJSON)
-            asignarImagen(resultadoJSON);
+            asignarDatos(resultadoJSON);
 
         })
         .catch(error => console.error("error", error));
 }
 
+function cambiarPokemon($seleccion) {
+    $seleccion.onclick = function(e) {
+        const $elemento = e.target
+        
+    }
+}
+
+let $seleccion = document.querySelectorAll(".lista-pokemon") 
+
+
 datosPrimeraPagina()
+cambiarPokemon($seleccion) 
+
 
 
 
@@ -116,19 +102,3 @@ document.querySelector("#ultima-pagina").onclick = ultimaPagina
 
 
 
-/*
-
-function asignarImagenes(resultado){
-    let imagen = document.querySelectorAll(".pokemones-imagen")
-    imagen.forEach((element, index) => {
-        let link = resultado.results[index].url;
-        fetch(link)
-        .then(resultado1 => resultado1.json())
-        .then(resultado1JSON => {
-            element.src = resultado1JSON.sprites.front_default
-        })
-        .catch(error => console.error("error", error))
-    })
-}
-
-*/
