3820a96406e3eb1d9ff74a502a52dbff4b8e4f84
warthundervlk <warthundervlk@gmail.com>
2021-10-25 22:16:21 -0300

    Arreglos en los botones
    Se modifican las funciones de los botones para que aparezcan o desaparezcan dependiendo la pagina

--- src/funciones_botones.js
+++ src/funciones_botones.js
@@ -1,8 +1,31 @@
 function anteriorPagina() {
-    if(Number(document.querySelector("#numero-pagina-comienzo").innerText) === 1) {
-        return alert("Estas en la primera pagina")
-    } else {
-        let pagina = (Number(document.querySelector("#numero-pagina-comienzo").innerText) - 1) * 20
+    if(Number(document.querySelector("#numero-pagina-comienzo").innerText) === 2) {
+        document.querySelector(".pagina-anterior").style.display = "none"
+    } else if(Number(document.querySelector("#numero-pagina-comienzo").innerText) === 44){
+        document.querySelector(".pagina-siguiente").style.display = "inline-block"
+    }
+
+    let pagina = (Number(document.querySelector("#numero-pagina-comienzo").innerText) - 1) * 20
+    fetch(`https://pokeapi-215911.firebaseapp.com/api/v2/pokemon?limit=20&offset=${pagina}`)
+        .then(resultado => resultado.json())
+        .then(resultadoJSON => {
+            let cantidadPokemones = resultadoJSON.results.length
+            crearListaPokemones(cantidadPokemones)
+            asignarNombres(resultadoJSON);
+            asignarDatos(resultadoJSON);
+            document.querySelector("#numero-pagina-comienzo").innerText = (pagina / 20) 
+            document.querySelector("#numero-pagina-final").innerText = (pagina / 20) 
+
+        })
+        .catch(error => console.error("error", error));
+    
+}
+
+function siguientePagina() {
+    if(Number(document.querySelector("#numero-pagina-comienzo").innerText) === 43) {
+        document.querySelector(".pagina-siguiente").style.display = "none"
+        document.querySelector(".pagina-anterior").style.display = "inline-block"
+        let pagina = Number(document.querySelector("#numero-pagina-comienzo").innerText) * 20
         fetch(`https://pokeapi-215911.firebaseapp.com/api/v2/pokemon?limit=20&offset=${pagina}`)
             .then(resultado => resultado.json())
             .then(resultadoJSON => {
@@ -10,18 +33,13 @@ function anteriorPagina() {
                 crearListaPokemones(cantidadPokemones)
                 asignarNombres(resultadoJSON);
                 asignarDatos(resultadoJSON);
-                document.querySelector("#numero-pagina-comienzo").innerText = (pagina / 20) 
-                document.querySelector("#numero-pagina-final").innerText = (pagina / 20) 
+                document.querySelector("#numero-pagina-comienzo").innerText = (pagina / 20) + 1
+                document.querySelector("#numero-pagina-final").innerText = (pagina / 20) + 1
     
             })
             .catch(error => console.error("error", error));
-    }
-}
-
-function siguientePagina() {
-    if(Number(document.querySelector("#numero-pagina-comienzo").innerText) === 45) {
-        return alert("Estas en la ultima pagina")
     } else {
+        document.querySelector(".pagina-anterior").style.display = "inline-block"
         let pagina = Number(document.querySelector("#numero-pagina-comienzo").innerText) * 20
         fetch(`https://pokeapi-215911.firebaseapp.com/api/v2/pokemon?limit=20&offset=${pagina}`)
             .then(resultado => resultado.json())
@@ -41,6 +59,8 @@ function siguientePagina() {
 }
 
 function ultimaPagina() {
+    document.querySelector(".pagina-anterior").style.display = "inline-block"
+    document.querySelector(".pagina-siguiente").style.display = "none"
     fetch(`https://pokeapi-215911.firebaseapp.com/api/v2/pokemon?limit=18&offset=880`)
         .then(resultado => resultado.json())
         .then(resultadoJSON => {
@@ -48,8 +68,8 @@ function ultimaPagina() {
             crearListaPokemones(cantidadPokemones)
             asignarNombres(resultadoJSON); 
             asignarDatos(resultadoJSON);
-            document.querySelector("#numero-pagina-comienzo").innerText = 45
-            document.querySelector("#numero-pagina-final").innerText = 45
+            document.querySelector("#numero-pagina-comienzo").innerText = 44
+            document.querySelector("#numero-pagina-final").innerText = 44
         })
         .catch(error => console.error("error", error));
 }
@@ -58,6 +78,6 @@ function ultimaPagina() {
 
 
 
-document.querySelector("#pagina-siguiente").onclick = siguientePagina
-document.querySelector("#pagina-anterior").onclick = anteriorPagina
-document.querySelector("#ultima-pagina").onclick = ultimaPagina
+document.querySelector(".pagina-siguiente").onclick = siguientePagina
+document.querySelector(".pagina-anterior").onclick = anteriorPagina
+document.querySelector(".ultima-pagina").onclick = ultimaPagina
