2e451a8f823d850ce2494950f35a8a61472c50ef
warthundervlk <warthundervlk@gmail.com>
2021-10-30 05:20:53 -0300

    Problema con los modulos

--- src/frases.js
+++ src/frases.js
@@ -1,24 +1,20 @@
-let frases = {
-    1: ["James", "No hay nada más fuerte que una amistad forjada en batalla."],
-    2: ["Profesor Oak" ,"Al que madruga Dios lo ayuda, o en este caso, consigue un Pokémon."],
-    3: ["Meowth", "Tenemos mucho en común, la misma tierra, el mismo aire, el mismo cielo, quizá si empezáramos a mirar lo que tenemos en común en vez de lo que nos hace diferentes, bueno, quién sabe..."],
-    4: ["Misty","Psyduck, ¡eres tan tonto que no sabes lo tonto que eres!"],
-    5: ["Mewtwo", "Las circunstancias en las que uno nace no tienen importancia, es lo que uno hace con el don de la vida lo que nos dice quiénes somos."],
-    6: ["Rayquaza", "Hazte cargo de tu destino."],
-    7: ["Grovyle", "Lo importante no es cuánto tiempo vives. Es lo que logras con tu vida."],
-    8: [" Ash Ketchum", "Todo el mundo se equivoca de vez en cuando."],
-    9: ["Ash Ketchum", "Verás, a veces los amigos tienen que irse, pero una parte de ellos se queda contigo."],
-    10: ["Mariscal", "Incluso si pierdes en la batalla, si superas lo que has hecho antes, te has superado a ti mismo."]
+export const listadoFrases = {
+  1: ['James', 'No hay nada más fuerte que una amistad forjada en batalla.'],
+  2: ['Profesor Oak', 'Al que madruga Dios lo ayuda, o en este caso, consigue un Pokémon.'],
+  3: ['Meowth', 'Tenemos mucho en común, la misma tierra, el mismo aire, el mismo cielo, quizá si empezáramos a mirar lo que tenemos en común en vez de lo que nos hace diferentes, bueno, quién sabe...'],
+  4: ['Misty', 'Psyduck, ¡eres tan tonto que no sabes lo tonto que eres!'],
+  5: ['Mewtwo', 'Las circunstancias en las que uno nace no tienen importancia, es lo que uno hace con el don de la vida lo que nos dice quiénes somos.'],
+  6: ['Rayquaza', 'Hazte cargo de tu destino.'],
+  7: ['Grovyle', 'Lo importante no es cuánto tiempo vives. Es lo que logras con tu vida.'],
+  8: [' Ash Ketchum', 'Todo el mundo se equivoca de vez en cuando.'],
+  9: ['Ash Ketchum', 'Verás, a veces los amigos tienen que irse, pero una parte de ellos se queda contigo.'],
+  10: ['Mariscal', 'Incluso si pierdes en la batalla, si superas lo que has hecho antes, te has superado a ti mismo.'],
+};
+
+export function asignarFrases(frases) {
+  const textoFrase = document.querySelector('#frase');
+  const autorFrase = document.querySelector('#frase-autor');
+  const numeroRandom = Math.ceil(Math.random() * 10);
+  textoFrase.textContent = frases[`${numeroRandom}`][1];
+  autorFrase.textContent = frases[`${numeroRandom}`][0];
 }
-
-function asignarFrases(frases) {
-    let textoFrase = document.querySelector("#frase")
-    let autorFrase = document.querySelector("#frase-autor")
-    let numeroRandom = Math.ceil(Math.random() * 10)
-    textoFrase.textContent = frases[`${numeroRandom}`][1]
-    autorFrase.textContent = frases[`${numeroRandom}`][0]
-}
-
-asignarFrases(frases)
-
-
--- src/main.js
+++ src/main.js
@@ -1,133 +1,75 @@
 /// <reference types="jquery"/>
 
-function asignarNombres(resultado){
-    let nombre = document.querySelectorAll(".nombre-pokemon")
-    nombre.forEach((element, index) => {
-        element.textContent = resultado.results[index].name.toUpperCase()
+import { listadoFrases, asignarFrases } from './frases';
+import { asignarDatosPokemon, crearListaPokemones, asignarNombresListado } from './ui';
+
+function asignarDatosPokemonSeleccionado(resultado) {
+  const link = resultado.results[0].url;
+  const linkMejorado = link.replace('https://pokeapi.co/api/v2', 'https://pokeapi-215911.firebaseapp.com/api/v2');
+  fetch(linkMejorado)
+    .then((resultado1) => resultado1.json())
+    .then((resultado1JSON) => {
+      asignarDatosPokemon(resultado1JSON);
     })
+    .catch((error) => console.error('error', error));
 }
 
-function asignarNombrePokemon(resultadoJSON){
-    let nombre = document.querySelector("#especificaciones-nombre")
-    nombre.textContent = resultadoJSON.name.toUpperCase()
+function inicio(pagina = 0) {
+  asignarFrases(listadoFrases);
+  fetch(`https://pokeapi-215911.firebaseapp.com/api/v2/pokemon?limit=20&offset=${pagina}`)
+    .then((resultado) => resultado.json())
+    .then((resultadoJSON) => {
+      const cantidadPokemones = resultadoJSON.results.length;
+      crearListaPokemones(cantidadPokemones);
+      asignarNombresListado(resultadoJSON);
+      asignarDatosPokemonSeleccionado(resultadoJSON);
+    })
+    .catch((error) => console.error('error', error));
 }
 
-function asignarCodigo(resultado){
-    let codigo = document.querySelector("#especificaciones-codigo-valor")
-    codigo.textContent = resultado.id
+function anteriorPagina() {
+  if (Number(document.querySelector('#numero-pagina-comienzo').innerText) === 2) {
+    document.querySelector('.pagina-anterior').style.display = 'none';
+  } else if (Number(document.querySelector('#numero-pagina-comienzo').innerText) === 44) {
+    document.querySelector('.pagina-siguiente').style.display = 'inline-block';
+  }
+
+  const pagina = (Number(document.querySelector('#numero-pagina-comienzo').innerText) - 1) * 20;
+  document.querySelector('#numero-pagina-comienzo').innerText = (pagina / 20);
+  document.querySelector('#numero-pagina-final').innerText = (pagina / 20);
+  inicio(pagina);
 }
 
-function asignarImagen(resultado) {
-    let $imagen = document.querySelector("#especificaciones-imagen")
-    $imagen.src = resultado.sprites.front_default
+function ultimaPagina() {
+  document.querySelector('.pagina-anterior').style.display = 'inline-block';
+  document.querySelector('.pagina-siguiente').style.display = 'none';
+  fetch('https://pokeapi-215911.firebaseapp.com/api/v2/pokemon?limit=18&offset=880')
+    .then((resultado) => resultado.json())
+    .then((resultadoJSON) => {
+      const cantidadPokemones = resultadoJSON.results.length;
+      crearListaPokemones(cantidadPokemones);
+      asignarListadoNombres(resultadoJSON);
+      asignarDatosPokemonSeleccionado(resultadoJSON);
+      document.querySelector('#numero-pagina-comienzo').innerText = 44;
+      document.querySelector('#numero-pagina-final').innerText = 44;
+    })
+    .catch((error) => console.error('error', error));
 }
 
-function asignarDatosPokemon(resultado){
-    let $spanTipo = document.querySelector("#especificaciones-tipo-valor")
-    let $spanAltura = document.querySelector("#especificaciones-altura-valor")
-    let $spanVida = document.querySelector("#especificaciones-vida-valor")
-    let $spanAtaque = document.querySelector("#especificaciones-ataque-valor")
-    let $spanDefensa = document.querySelector("#especificaciones-defensa-valor")
-    let $spanPeso = document.querySelector("#especificaciones-peso-valor")
-    let $spanAtaqueEspecial = document.querySelector("#especificaciones-ataque-especial-valor")
-    let $spanDefensaEspecial = document.querySelector("#especificaciones-defensa-especial-valor")
-    let $spanVelocidad = document.querySelector("#especificaciones-velocidad-valor")
-
-    $spanAltura.textContent = resultado.height
-    $spanVida.textContent = resultado.stats[0].base_stat
-    $spanAtaque.textContent = resultado.stats[1].base_stat
-    $spanDefensa.textContent = resultado.stats[2].base_stat
-    $spanPeso.textContent = resultado.weight
-    $spanAtaqueEspecial.textContent = resultado.stats[3].base_stat
-    $spanDefensaEspecial.textContent = resultado.stats[4].base_stat
-    $spanVelocidad.textContent = resultado.stats[5].base_stat
-
-    let texto = ""
-
-   resultado.types.forEach(elemento => {
-        texto = texto + " " + elemento.type.name[0].toUpperCase() + elemento.type.name.slice(1)
-    } )
-    
-    $spanTipo.textContent = texto
+function siguientePagina() {
+  if (Number(document.querySelector('#numero-pagina-comienzo').innerText) === 43) {
+    ultimaPagina();
+  } else {
+    document.querySelector('.pagina-anterior').style.display = 'inline-block';
+    const pagina = Number(document.querySelector('#numero-pagina-comienzo').innerText);
+    document.querySelector('#numero-pagina-comienzo').innerText = pagina + 1;
+    document.querySelector('#numero-pagina-final').innerText = pagina + 1;
+    inicio((pagina + 1) * 20);
+  }
 }
 
-function crearListaPokemones(cantidad){
-    if(document.querySelector("ul")){
-        document.querySelector("ul").remove()
-    }
-    let $col = document.querySelector("#lista")
-    let $ul = document.createElement("ul")
-    for(let i=1; i <= cantidad; i++){
-        let $li = document.createElement("li")
-        $li.className = "lista-pokemon"
-        $li.id = `lista-pokemon-${i}`
-        $img = document.createElement("img")
-        $img.className = "poke-bola-imagen"
-        $img.src = "Img/Poké_Ball_icon.svg.png"
-        $img.alt = "poke-bola"
-        $li.appendChild($img)
-        let $span = document.createElement("span")
-        $span.className = "nombre-pokemon"
-        $li.appendChild($span)
-        $ul.appendChild($li)
-    }
-    $col.appendChild($ul)
-    document.querySelector("#lista-pokemon-1").className = "lista-pokemon seleccionado"
+document.querySelector('.pagina-siguiente').onclick = siguientePagina;
+document.querySelector('.pagina-anterior').onclick = anteriorPagina;
+document.querySelector('.ultima-pagina').onclick = ultimaPagina;
 
-    seleccionarPokemones(cantidad)
-}
-
-
-function asignarDatos(resultado){
-    
-        let link = resultado.results[0].url;
-        let linkMejorado = link.replace("https://pokeapi.co/api/v2", "https://pokeapi-215911.firebaseapp.com/api/v2")
-        fetch(linkMejorado)
-        .then(resultado1 => resultado1.json())
-        .then(resultado1JSON => {
-            asignarImagen(resultado1JSON)
-            asignarCodigo(resultado1JSON)
-            asignarDatosPokemon(resultado1JSON)
-            asignarNombrePokemon(resultado1JSON)
-        })
-        .catch(error => console.error("error", error))
-}
-
-
-
-function datosPrimeraPagina() {
-    fetch("https://pokeapi-215911.firebaseapp.com/api/v2/pokemon?limit=20&offset=0")
-        .then(resultado => resultado.json())
-        .then(resultadoJSON => {
-            let cantidadPokemones = resultadoJSON.results.length
-            crearListaPokemones(cantidadPokemones)
-            asignarNombres(resultadoJSON);
-           
-            asignarDatos(resultadoJSON);
-        })
-        .catch(error => console.error("error", error));
-}
-
-function cambiarPokemon(seleccion) {
-
-        fetch(`https://pokeapi-215911.firebaseapp.com/api/v2/pokemon/${seleccion}`)
-        .then(resultado => resultado.json())
-        .then(resultadoJSON => {
-            asignarCodigo(resultadoJSON)
-            asignarNombrePokemon(resultadoJSON)
-            asignarDatosPokemon(resultadoJSON);
-            asignarImagen(resultadoJSON)
-
-        })
-        .catch(error => console.error("error", error));
-}
-
-
-datosPrimeraPagina()
-
-
-
-
-
-
-
+inicio();
