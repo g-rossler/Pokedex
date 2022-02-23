12fdc8adf0cc9fb2504527aaf412607cdf387b31
warthundervlk <warthundervlk@gmail.com>
2022-02-23 06:11:13 -0300

    Arreglo de error en las frases presentadas

--- src/frases.js
+++ src/frases.js
@@ -45,6 +45,6 @@ export function asignarFrases(frases) {
   const textoFrase = document.querySelector('#frase');
   const autorFrase = document.querySelector('#frase-autor');
   const numeroRandom = Math.floor(Math.random() * 10);
-  textoFrase.textContent = frases[numeroRandom].autor;
-  autorFrase.textContent = frases[numeroRandom].frase;
+  autorFrase.textContent = frases[numeroRandom].autor;
+  textoFrase.textContent = frases[numeroRandom].frase;
 }
