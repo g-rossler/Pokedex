596d01ae047e626c92425ce15ff6f6ecd6fe9858
warthundervlk <warthundervlk@gmail.com>
2022-03-10 16:35:17 -0300

    Se crea el archivo Servicios
    En el archivo se encuentran las funciones que se llaman cuando se cargan los pokemones y el pokemon en especifico

---
+++ src/servicios.js
@@ -0,0 +1,40 @@
+import {
+  cargarPokemonesLocalStorage, guardarPokemonesLocalStorage, cargarPokemonLocalStorage,
+  guardarPokemonLocalStorage,
+  // eslint-disable-next-line import/extensions
+} from './storage.js';
+// eslint-disable-next-line import/extensions
+import { cargarPokemonesAPI, cargarPokemonAPI } from './api.js';
+
+export async function cargarPokemon() {
+  const nombre = document.querySelectorAll('.lista-pokemon')[0].innerText.toLowerCase();
+  let datos;
+  try {
+    datos = cargarPokemonLocalStorage(nombre);
+  } catch (e) {
+    datos = await cargarPokemonAPI(nombre);
+    guardarPokemonLocalStorage(nombre, datos);
+  }
+  return datos;
+}
+
+export async function cargarPokemones(limite, pagina) {
+  try {
+    return cargarPokemonesLocalStorage(limite, pagina);
+  } catch (e) {
+    const pokemones = await cargarPokemonesAPI(limite, pagina);
+    guardarPokemonesLocalStorage(limite, pagina, pokemones);
+    return pokemones;
+  }
+}
+
+export async function cambiarPokemon(nombre) {
+  let resultado;
+  try {
+    resultado = cargarPokemonLocalStorage(nombre);
+  } catch (e) {
+    resultado = await cargarPokemonAPI(nombre);
+    guardarPokemonLocalStorage(nombre, resultado);
+  }
+  return resultado;
+}
