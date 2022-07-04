cc5d68dd635911c04c90b9a7c6c978c860a0d026
Gerardo Rossler <rossler.gerardo.dev@gmail.com>
2022-07-04 16:56:28 -0300

    Mejorar nombres de variables

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export async function cargarPokemonesAPI(limite, pagina) {
   const link = await fetch(`https://pokeapi-215911.firebaseapp.com/api/v2/pokemon?limit=${limite}&offset=${pagina}`);
-  const respuesta = await link.json();
-  return respuesta;
+  const respuestaJSON = link.json();
+  return respuestaJSON;
 }
 
 export async function cargarPokemonAPI(nombre) {
   const link = await fetch(`https://pokeapi-215911.firebaseapp.com/api/v2/pokemon/${nombre}`);
-  const respuesta = link.json();
-  return respuesta;
+  const respuestaJSON = link.json();
+  return respuestaJSON;
 }
--- src/eventos.js
+++ src/eventos.js
@@ -1,25 +1,28 @@
-import inicio from './main.js';
-import { verificarVisibilidadBotonesPaginador, modificarNumeroPagina } from './ui.js';
+import { crearPaginaPokedex } from "./main.js";
+import {
+  verificarVisibilidadBotonesPaginador,
+  modificarNumeroPagina,
+} from "./ui.js";
 
-const $botonAnteriorPagina = document.querySelector('.anterior-pagina');
-const $botonSiguientePagina = document.querySelector('.siguiente-pagina');
-const $botonUltimaPagina = document.querySelector('.ultima-pagina');
+const $botonAnteriorPagina = document.querySelector(".anterior-pagina");
+const $botonSiguientePagina = document.querySelector(".siguiente-pagina");
+const $botonUltimaPagina = document.querySelector(".ultima-pagina");
 
 function averiguarPagina() {
-  const $numeroPagina = document.querySelector('#numero-pagina');
+  const $numeroPagina = document.querySelector("#numero-pagina");
   return Number($numeroPagina.innerText);
 }
 
 function cambiarPagina(texto, numeroPagina) {
-  if (texto === 'Pagina Siguiente') {
-    modificarNumeroPagina('mas');
-    inicio((numeroPagina) * 20);
-  } else if (texto === 'Pagina Anterior') {
-    modificarNumeroPagina('menos');
-    inicio((numeroPagina - 2) * 20);
+  if (texto === "Pagina Siguiente") {
+    modificarNumeroPagina("mas");
+    crearPaginaPokedex(numeroPagina * 20);
+  } else if (texto === "Pagina Anterior") {
+    modificarNumeroPagina("menos");
+    crearPaginaPokedex((numeroPagina - 2) * 20);
   } else {
     modificarNumeroPagina();
-    inicio(1120);
+    crearPaginaPokedex(1120);
   }
 }
 
@@ -31,15 +34,15 @@ function manejarCambioPagina(texto) {
 }
 
 export default function activarPaginador() {
-  $botonSiguientePagina.addEventListener('click', (e) => {
+  $botonSiguientePagina.addEventListener("click", (e) => {
     manejarCambioPagina(e.target.innerText);
   });
 
-  $botonAnteriorPagina.addEventListener('click', (e) => {
+  $botonAnteriorPagina.addEventListener("click", (e) => {
     manejarCambioPagina(e.target.innerText);
   });
 
-  $botonUltimaPagina.addEventListener('click', (e) => {
+  $botonUltimaPagina.addEventListener("click", (e) => {
     manejarCambioPagina(e.target.innerText);
   });
 }
